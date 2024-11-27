854641c6ebb56c329da1a5db10785504277cc951
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-27 10:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function indexOfIgnoreCase(s1, s2) {
 	return -1;
 }
 // Please do not change the code below
-//const s1 = prompt("Enter s1:");
-//const s2 = prompt("Enter s2:");
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
